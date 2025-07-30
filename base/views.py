679cd11f467c921958c757d01531dee6079ca098
Moh-Ent <ent.moh127@gmail.com>
2025-07-30 21:48:25 +0430

    template route modify
    added templates in the og route

--- base/views.py
+++ base/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def homepage(request):
-    return render(request, 'homepage_sheet.html')
+    return render(request, 'base/homepage_sheet.html')
 
 def aboutpage(request):
-    return render(request, 'about_sheet.html')
+    return render(request, 'base/about_sheet.html')
 
 def contactpage(request):
-    return render(request, 'contact_sheet.html')
+    return render(request, 'base/contact_sheet.html')
