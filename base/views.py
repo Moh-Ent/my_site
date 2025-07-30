634712db92735ae9855a2a13d325f87251ad9936
Moh-Ent <ent.moh127@gmail.com>
2025-07-30 13:29:45 +0430

    Working with 'base' app
    making different urls and funcs related to them in views.py all in 'base' app

--- base/views.py
+++ base/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def homepage(request):
+    return HttpResponse('<h1>Whatssup?</h1>')
+
+def aboutpage(request):
+    return HttpResponse('<h1>Just a chill guy</h1>')
+
+def contactpage(request):
+    return HttpResponse('<h1>Sukoma</h1>')
--- mysite/views.py
+++
@@ -1,3 +0,0 @@
-from django.http import HttpResponse
-def testi(request):
-    return HttpResponse('<h1>business is business</h1>')
