043baf3794064680dadd4672fa53ac8ca3f4a8dd
Moh-Ent <ent.moh127@gmail.com>
2025-07-31 20:48:37 +0430

    inserting and fitting templates and statics
    inserting and fitting templates and statics just the index html

--- base/views.py
+++ base/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
 
 def homepage(request):
-    return render(request, 'base/homepage_sheet.html')
+    return render(request, 'base/index.html')
 
 def aboutpage(request):
-    return render(request, 'base/about_sheet.html')
+    return render(request, 'base/about.html')
 
 def contactpage(request):
-    return render(request, 'base/contact_sheet.html')
+    return render(request, 'base/contact.html')
