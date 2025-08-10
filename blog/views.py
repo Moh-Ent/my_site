e6ca166e54c12a9b16ccebe6c6d3ae6c7a600e39
Moh-Ent <ent.moh127@gmail.com>
2025-08-10 11:53:44 +0430

    working with post database
    working with post database and showing posts in the blog page

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from blog.models import Post
 
 # Create your views here.
 def bloghomepage(request):
-    return render(request, 'blog/blog-home.html')
+    posts = Post.objects.filter(is_published=1)
+    context = {'posts' : posts}
+    return render(request, 'blog/blog-home.html', context)
 
 def blogdetailpage(request):
     return render(request, 'blog/blog-single.html')
